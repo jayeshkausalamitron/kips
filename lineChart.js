83cc59f7bf65d90334aad016f0412c03388b8434
jayeshkausalamitron <163313351+jayeshkausalamitron@users.noreply.github.com>
2024-05-06 22:29:15 +0530

    Add files via upload
    latest update

--- lineChart.js
+++ lineChart.js
@@ -311,3 +311,290 @@ $(document).ready(function() {
       }
     });
   });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('onTimeDeliveryPerCharlineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'On-time delivery percentage',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('onTimeDeliveryPerCharlineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'On-time delivery percentage',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('avgSlineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Average lead-time for release',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('laborCostPerPanellineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Labor cost per panel Hours Value in $',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('laborCostPerPanellineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Labor cost per panel Hours Value in $',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('laborHourPerPanellineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Labor hours per panel Hours Value in $',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('overTimePerlineChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Overtime percentage',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('panelPerMonthCountValue').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Panels per month Number count Value in $',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('squrePerMonthCountValue').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Panel sq/ft per month',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('totalAvgPanelPriceChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Total average Panel price Value in $',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    let ctx = document.getElementById('avgCycleTimebyLayerChart').getContext('2d');
+    let lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000'],
+        datasets: [{
+          label: 'Average Panel price USA',
+          data: [16, 12, 23, 54, 45, 56, 7, 28, 19, 50,],
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderWidth: 2,
+          fill: true
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
